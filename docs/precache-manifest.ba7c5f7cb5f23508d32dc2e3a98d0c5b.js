6d3e40d94c1d5404302f6b16fe17a2666900ba70
smartzmx <smartzmx@gmail.com>
2020-07-03 17:13:53 -0500

    subiendo archivos de build

---
+++ docs/precache-manifest.ba7c5f7cb5f23508d32dc2e3a98d0c5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd0dd23d911226f329d2723c7b57679",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2242520e220c7efd8c39",
+    "url": "/static/css/main.2122be69.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2242520e220c7efd8c39",
+    "url": "/static/js/main.63b4ddcd.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
